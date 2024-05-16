50f8d72d855ff19252c78d06e7b4b74915abb152
mynewaccount1 <64211873+mynewaccount1@users.noreply.github.com>
2024-05-16 11:13:32 +0530

    Update HelloWorldApplication.java
    Message added by developer 2

--- src/main/java/com/rsr/HelloWorldApplication.java
+++ src/main/java/com/rsr/HelloWorldApplication.java
@@ -8,7 +8,7 @@ public class HelloWorldApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloWorldApplication.class, args);
-		System.out.println("Hi Welcome to Hello World..!!");
+		System.out.println("Hi Welcome Ramesh!");
 	}
 
 }
